d17508d9439fe0aa43f82a244622882d98fa7320
Marsha Annon <marshaannon@gmail.com>
2020-03-04 13:31:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,18 @@ def countdown(number="countdown_output")
     number = 10
     while number > 0
       puts "#{number} SECOND(S)!"
-      sleep 5
       number -= 1
     end
    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  (number="countdown_output")
+      number = 10
+      while number > 0
+        puts "#{number} SECOND(S)!"
+        sleep 5
+        number -= 1
+      end
+     "HAPPY NEW YEAR!"
   end
