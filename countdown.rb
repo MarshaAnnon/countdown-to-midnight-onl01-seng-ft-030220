71b8058670377f599874950497278f662cde651d
Marsha Annon <marshaannon@gmail.com>
2020-03-04 13:06:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
-
-def countdown
-end
+def countdown(number="countdown_output")
+    number = 10
+    sleep 5
+    while number > 0
+      puts "#{number} SECOND(S)!"
+      number -= 1
+    end
+   "HAPPY NEW YEAR!"
+  end
