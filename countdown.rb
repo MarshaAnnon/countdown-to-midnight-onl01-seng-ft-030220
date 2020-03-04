e9898222b3cc6d2e5fd8f2ac3ab29964ba268c01
Marsha Annon <marshaannon@gmail.com>
2020-03-04 13:10:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,11 @@
 #write your code here
 def countdown(number="countdown_output")
     number = 10
-    sleep 5
     while number > 0
       puts "#{number} SECOND(S)!"
       number -= 1
+      sleep 5.seconds
     end
    "HAPPY NEW YEAR!"
   end
+  
